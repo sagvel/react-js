9da9c910c9489f065a23874710d9dbd7d6cce927
sagvel <saulko.oleksander@gmail.com>
2022-07-17 14:09:18 +0300

    add todo-init solution

---
+++ todo-list/src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './styles.css';
+
+const todoList = <h1 class="title">Todo List</h1>;
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(todoList);
