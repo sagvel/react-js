6d793d3cf11c022bd99ef4b9a39efbd3fd5f9fe1
sagvel <saulko.oleksander@gmail.com>
2022-07-17 13:43:18 +0300

    correct css 3.1

--- react_intro/03_first_react_app_with_jsx/index.js
+++ react_intro/03_first_react_app_with_jsx/index.js
@@ -1,26 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './styles.css';
 
 const rootElement = document.querySelector('#root');
-
-// const renderGreeting = (elem) => {
-//   const containerElem = document.createElement('div');
-//   containerElem.classList.add('greeting');
-
-//   const titleElem = document.createElement('h1');
-//   titleElem.classList.add('greeting__title');
-//   titleElem.textContent = 'Hello, world!';
-
-//   const textElem = document.createElement('p');
-//   textElem.classList.add('greeting__text');
-//   textElem.textContent = 'I am learning React';
-
-//   containerElem.append(titleElem, textElem);
-
-//   elem.append(containerElem);
-// };
-
-// renderGreeting(rootElement);
 
 const renderGreeting = (
   <div className="greeting">
