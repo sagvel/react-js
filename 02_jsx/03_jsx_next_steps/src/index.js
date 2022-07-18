b9f42df2578f5bb35fe990fe6a1871521e8def7b
sagvel <saulko.oleksander@gmail.com>
2022-07-18 14:49:29 +0300

    add 2.3 solution

---
+++ 02_jsx/03_jsx_next_steps/src/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './styles.css';
+
+const rootElem = document.querySelector('#root');
+
+const renderSeconds = (time) => {
+  const seconds = new Date(time).getSeconds();
+  const textColor = seconds % 2 !== 0 ? '#fff' : '#000';
+  const backgroundColor = seconds % 2 === 0 ? '#fff' : '#000';
+  const style = {
+    color: textColor,
+    backgroundColor,
+  };
+
+  const timerElem = (
+    <div className="seconds" style={style}>
+      {seconds}
+    </div>
+  );
+
+  ReactDOM.render(timerElem, rootElem);
+};
+
+setInterval(() => renderSeconds(new Date()), 1000);
