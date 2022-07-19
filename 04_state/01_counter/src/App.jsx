0967b20d0b852cdc5d6b0b1f2556a9a938db7218
sagvel <saulko.oleksander@gmail.com>
2022-07-19 15:55:41 +0300

    add lifecikle methods 4.1

--- 04_state/01_counter/src/App.jsx
+++ 04_state/01_counter/src/App.jsx
@@ -4,9 +4,9 @@ import Counter from './Counter';
 export default function App() {
   return (
     <>
-      <Counter start={10} intertval={3000} />
-      {/* <Counter start={23} intertval={2000} />
-      <Counter start={-67} intertval={1000} /> */}
+      <Counter start={10} intertval={1000} />
+      <Counter start={23} intertval={500} />
+      <Counter start={-67} intertval={100} />
     </>
   );
 }
--- 04_state/01_counter/src/Counter.jsx
+++ 04_state/01_counter/src/Counter.jsx
@@ -2,18 +2,21 @@ import React, { Component } from 'react';
 import './counter.scss';
 
 export default class Counter extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      counter: props.start,
-    };
-
-    setInterval(() => {
+  state = {
+    counter: this.props.start,
+  };
+  componentDidMount() {
+    this.timerId = setInterval(() => {
       this.setState({
         counter: this.state.counter + 1,
       });
-    }, props.intertval);
+    }, this.props.intertval);
   }
+
+  componentWillUnmount() {
+    clearInterval(this.timerId);
+  }
+
   render() {
     return <div className="counter">{this.state.counter}</div>;
   }
