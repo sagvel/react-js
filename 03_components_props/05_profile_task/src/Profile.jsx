14017160caa5326ee3794ada89cb1ac28b3053d1
sagvel <saulko.oleksander@gmail.com>
2022-07-19 13:22:19 +0300

    add 3.5 solution

---
+++ 03_components_props/05_profile_task/src/Profile.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import moment from 'moment';
+
+export default function Profile(props) {
+  const formatDate = (date) => moment(date).format('DD MMM YYYY');
+  console.log(props);
+  return (
+    <div className="profile">
+      <div className="profile__name">
+        {props.userData.firstName} {props.userData.lastName}
+      </div>
+      <div className="profile__birth">
+        Was born {formatDate(props.userData.birthDate)} in{' '}
+        {props.userData.birthPlace}
+      </div>
+    </div>
+  );
+}
--- 03_components_props/05_profile_task/src/index.jsx
+++ 03_components_props/05_profile_task/src/index.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
+import Profile from './Profile';
 
 const rootElem = document.querySelector('#root');
+const userData = {
+  firstName: 'James',
+  lastName: 'Bond',
+  birthDate: '1991-01-17T11:11:11.819Z',
+  birthPlace: 'London',
+};
 
 function App() {
-  return <div>App</div>;
+  return <Profile userData={userData} />;
 }
 
 ReactDOM.render(<App />, rootElem);
