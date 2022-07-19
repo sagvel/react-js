c35dcba30d0dfabb0560115100be7974c56f0b87
sagvel <saulko.oleksander@gmail.com>
2022-07-19 13:01:45 +0300

    3.5 init

---
+++ 03_components_props/05_profile_task/src/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.scss';
+
+const rootElem = document.querySelector('#root');
+
+function App() {
+  return <div>App</div>;
+}
+
+ReactDOM.render(<App />, rootElem);
