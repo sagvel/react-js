2b29730d10a487b0d3cedbc3f9c4b73374ac117c
sagvel <saulko.oleksander@gmail.com>
2022-07-19 15:43:11 +0300

    remove 2 components

--- 04_state/01_counter/src/App.jsx
+++ 04_state/01_counter/src/App.jsx
@@ -5,8 +5,8 @@ export default function App() {
   return (
     <>
       <Counter start={10} intertval={3000} />
-      <Counter start={23} intertval={2000} />
-      <Counter start={-67} intertval={1000} />
+      {/* <Counter start={23} intertval={2000} />
+      <Counter start={-67} intertval={1000} /> */}
     </>
   );
 }
