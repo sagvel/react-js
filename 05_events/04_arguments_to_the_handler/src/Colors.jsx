cba484b6dbf3e2173b256603e885dafbf199e419
sagvel <saulko.oleksander@gmail.com>
2022-07-20 10:13:20 +0300

    add 5.4 solution

--- 05_events/04_arguments_to_the_handler/src/Colors.jsx
+++ 05_events/04_arguments_to_the_handler/src/Colors.jsx
@@ -1,17 +1,30 @@
 import React, { Component } from 'react';
 
+const RED = '#f00';
+const GREEN = '#0f0';
+const BLUE = '#00f';
+
 export default class Colors extends Component {
   setBackgroundColor = (color) => {
     document.body.style.backgroundColor = color;
   };
   render() {
-    const { color } = this.props;
     return (
       <div className="colors">
         <button
           className="colors__button"
-          style={{ backgroundColor: color }}
-          onClick={this.setBackgroundColor.bind(this, color)}
+          style={{ backgroundColor: RED }}
+          onClick={this.setBackgroundColor.bind(this, RED)}
+        ></button>
+        <button
+          className="colors__button"
+          style={{ backgroundColor: GREEN }}
+          onClick={this.setBackgroundColor.bind(this, GREEN)}
+        ></button>
+        <button
+          className="colors__button"
+          style={{ backgroundColor: BLUE }}
+          onClick={this.setBackgroundColor.bind(this, BLUE)}
         ></button>
       </div>
     );
