0efea392a228180bd6383ff53b26def121516a3b
sagvel <saulko.oleksander@gmail.com>
2022-07-24 17:04:34 +0300

    fix

--- 08_lifecycle_methods/01_lifecycle_methods_task/src/Life.jsx
+++ 08_lifecycle_methods/01_lifecycle_methods_task/src/Life.jsx
@@ -5,19 +5,23 @@ export default class Life extends Component {
     super();
     console.log('constructor: good place to create state');
   }
+
   componentDidMount() {
     console.log('componentDidMount: API calls, subscriptions');
   }
-  shouldComponentUpdate() {
-    console.log(
-      'shouldComponentUpdate(nextProps, nextState): decide to render or not to render'
-    );
-  }
+
   componentDidUpdate() {
     console.log(
       'componentDidUpdate(prevProps, prevState): some updates based on new props'
     );
   }
+
+  shouldComponentUpdate() {
+    console.log(
+      'shouldComponentUpdate(nextProps, nextState): decide to render or not to render'
+    );
+  }
+
   componentWillUnmount() {
     console.log(
       'componentWillUnmount(): cleanup before DOM related to component will be removed'
