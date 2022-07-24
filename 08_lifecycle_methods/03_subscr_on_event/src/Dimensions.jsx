b35772ecc4c2128d98f9cff7e75c56bcd806b1de
sagvel <saulko.oleksander@gmail.com>
2022-07-24 17:57:21 +0300

    add 8.3 solution

---
+++ 08_lifecycle_methods/03_subscr_on_event/src/Dimensions.jsx
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+
+export default class Dimensions extends Component {
+  state = {
+    width: null,
+    height: null,
+  };
+
+  componentDidMount() {
+    const { innerWidth, innerHeight } = window;
+    this.setDimensions(innerWidth, innerHeight);
+  }
+
+  componentDidUpdate() {
+    window.addEventListener('resize', this.onResize);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.onResize);
+  }
+
+  onResize = (e) => {
+    const { innerWidth, innerHeight } = e.target;
+    this.setDimensions(innerWidth, innerHeight);
+  };
+
+  setDimensions = (width, height) => {
+    this.setState({
+      width,
+      height,
+    });
+    document.title = `${innerWidth} x ${innerHeight}`;
+  };
+
+  render() {
+    const { width, height } = this.state;
+    return <div className="dimensions">{`${width}px - ${height}px`}</div>;
+  }
+}
